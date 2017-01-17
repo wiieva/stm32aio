305de22bb407858cb9ad78f2671135d01cfa1fd2
Oleg Gerasimov <ogerasimov@gmail.com>
2017-01-17 12:28:35 +0300

    [fix] reasonable usb-uart buffer size
    Signed-off-by: Oleg Gerasimov <ogerasimov@gmail.com>

--- src/usbuart/usbd_conf.h
+++ src/usbuart/usbd_conf.h
@@ -25,7 +25,7 @@
  #define CDC_CMD_PACKET_SZE             8    /* Control Endpoint Packet size */
 
  #define CDC_IN_FRAME_INTERVAL          5    /* Number of frames between IN transfers */
- #define APP_RX_DATA_SIZE               2048 /* Total size of IN buffer: 
+ #define APP_RX_DATA_SIZE               512  /* Total size of IN buffer: 
                                                 APP_RX_DATA_SIZE*8/MAX_BAUDARATE*1000 should be > CDC_IN_FRAME_INTERVAL */
 #endif /* USE_USB_OTG_HS */
 
